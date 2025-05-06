b4b01abbb56b588e72e38752838bcc1ca7eca87b
chappy <chappy.job.666@gmail.com>
2025-05-06 13:34:57 +0000

    add icons

--- task-app/src/app/layout/header/header.component.ts
+++ task-app/src/app/layout/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, Home, Settings} from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  // icons
+  readonly HomeIcon = Home;
+  readonly SettingsIcon = Settings;
+
   isMenuOpen = false;
 }
